c55098501db4a653c0d27c1db1b34e14d2484b01
Gh0st <otioritsejafor@gmail.com>
2018-08-14 09:02:04 -0400

    Button functionality and Images

--- ViewController.swift
+++ ViewController.swift
@@ -12,23 +12,38 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var recordingLabel: UILabel!
     
+    @IBOutlet weak var recordButton: UIButton!
+    
+    @IBOutlet weak var stopRecording: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        stopRecording.isEnabled = false;
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("ViewWillAppearCalled")
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
     @IBAction func recordAudio(_ sender: Any) {
-        print("record button was pressed")
         recordingLabel.text = "Recording in Progress"
+        stopRecording.isEnabled = true;
+        recordButton.isEnabled = false;
     }
     
     @IBAction func stopRecording(_ sender: Any) {
-        print("Button was pressed")
+        recordButton.isEnabled = true
+        stopRecording.isEnabled = false
+        recordingLabel.text = "Tap to Record"
     }
+    
+    
 }
 
